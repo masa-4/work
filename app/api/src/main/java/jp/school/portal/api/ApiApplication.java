6c62a1977f53bfe248d7119d5eb34003d4e7ebc2
masa-4 <monokesi47@gmail.com>
2024-09-17 20:46:36 +0900

    DB接続のためのクラスや接続情報を追加

--- app/api/src/main/java/jp/school/portal/api/ApiApplication.java
+++ app/api/src/main/java/jp/school/portal/api/ApiApplication.java
@@ -1,13 +1,26 @@
 package jp.school.portal.api;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "jp.school.portal.domain")
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	// アプリケーション起動時にJSTを設定
+	@PostConstruct
+	public void init() {
+			// タイムゾーンをJST（日本標準時）に設定
+			TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
 	}
 
 }
